fac956f33030ec9cb9c279da230590cc2986ef0b
r-elen <elen.ryazanova@gmail.com>
2022-12-18 22:00:59 +0300

    Reworked (сначала отрабатывает код, потом интерфейс)

--- doc/replace2.py
+++ doc/replace2.py
@@ -67,8 +67,7 @@ def repl_template(templ_name: str, file_name: str, temps: list, info: list):
     open_doc.save(file_name + '.docx')
 
 
-def main():
-
+def create_template_list():
     tmp_num_doc = re.compile(r"number")
     tmp_date_1 = re.compile(r"date")
     tmp_client_name = re.compile(r"fullname")
@@ -78,6 +77,10 @@ def main():
 
     list_templates = [tmp_num_doc, tmp_date_1, tmp_client_name, tmp_address, tmp_initials, tmp_tel]
 
+    return list_templates
+
+
+def input_info():
     doc_num = create_num(input("№ договора: "))
     date_1 = create_date()
     client_lastname = input("Фамилия: ")
@@ -89,10 +92,17 @@ def main():
     tel = input("Тел.: ")
 
     list_info = [doc_num, date_1, client_full_name, address, initials, tel]
+    return list_info
+
+
+def main():
+
+    list_temps = create_template_list()
+    list_info = input_info()
 
     tmp_doc = "Шаблон 1.docx"
-    name_save_doc = ' '.join(['Договор', doc_num])
-    repl_template(tmp_doc, name_save_doc, list_templates, list_info)
+    name_save_doc = ' '.join(['Договор', list_info[0]])
+    repl_template(tmp_doc, name_save_doc, list_temps, list_info)
 
 
 if __name__ == '__main__':
--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -1,6 +1,8 @@
-
+import re
 from PySide6 import QtWidgets
+
 from ui.form.MainForm import Ui_Form
+import doc.replace2 as r
 
 
 class Window(QtWidgets.QWidget):
@@ -8,9 +10,14 @@ class Window(QtWidgets.QWidget):
         super().__init__(parent)
 
         self.ui = Ui_Form()
-        self.ui.setupUi(self)
 
+        self.ui.setupUi(self)
         self.initUi()
+
+        self.input_info()
+        self.initSignals()
+
+
 
     def initUi(self) -> None:
         """
@@ -18,25 +25,56 @@ class Window(QtWidgets.QWidget):
         :return: None
         """
         self.setWindowTitle("DocCreator")
-        self.ui.client_lastname.setPlaceholderText("Иванов")
 
         # self.ui.but_savingdoc
 
-    def initChild(self) -> None:
-        """
-        Инициализация дочерних окон
-        :return: None
-        """
-        # self.win_2 = ChildWindow() #импорт окна
-        pass
+    # def initChild(self) -> None:
+    #     """
+    #     Инициализация дочерних окон
+    #     :return: None
+    #     """
+    #     # self.win_2 = ChildWindow() #импорт окна
+    #     pass
 
     def initSignals(self) -> None:
         """
         Инициализация сигнала
         :return: None
         """
-        # self.pb_1.clicked.connect(self.print_hello)
-        pass
+        self.ui.create_doc_event.clicked.connect(self.main())
+        # self.ui.button_savefolder.clicked.connect(self.show_dialog_window)
+
+    # def show_dialog_window(self) -> None:
+    #     """
+    #     Показ выбранного диалогового окна
+    #
+    #     :return: None
+    #     """
+    #     QtWidgets.QMessageBox.about(self, "Уведомление", "Обработка выполнена")
+
+    def input_info(self):
+        doc_num = r.create_num(self.ui.input_doc_num.text())  # получение данных из строки ввода пользователем
+        client_lastname = self.ui.input_client_lastname.text()
+        client_name = self.ui.input_client_name.text()
+        client_middname = self.ui.input_client_middname.text()
+        address = self.ui.input_address.text()
+        tel = self.ui.input_tel.text()
+        date_1 = r.create_date()
+        client_full_name = r.create_full_name(client_lastname, client_name, client_middname)
+        # initials = r.create_initials(client_lastname, client_name, client_middname)
+
+        list_info = [doc_num, date_1, client_full_name, address,  tel]
+
+        return list_info
+
+    def main(self):
+        list_temps = r.create_template_list()
+        list_info = self.input_info()
+        tmp_doc = "Шаблон 1.docx"
+        # name_save_doc = ' '.join(['Договор', list_info[0]])
+        name_save_doc = "Договор_test"
+
+        r.repl_template(tmp_doc, name_save_doc, list_temps, list_info)
 
 
 
