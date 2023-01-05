5e3e13e16409e080e3d06796eabf883b7d0d0327
r-elen <elen.ryazanova@gmail.com>
2023-01-05 22:47:59 +0300

    reworked TODO reworking find regex

--- doc/replace2.py
+++ doc/replace2.py
@@ -47,15 +47,15 @@ def create_initials(last_name: str, name: str, middle_name=None):
 
 
 def docx_replace_regex(doc_obj, regex, replace):
-
     for p in doc_obj.paragraphs:
-        if regex.search(p.text):
+        if regex.fullmatch(p.text):
             inline = p.runs
             # Loop added to work with runs (strings with same style)
             for i in range(len(inline)):
                 if regex.search(inline[i].text):
                     text = regex.sub(replace, inline[i].text)
                     inline[i].text = text
+                    print(text)
 
     for table in doc_obj.tables:
         for row in table.rows:
@@ -63,15 +63,33 @@ def docx_replace_regex(doc_obj, regex, replace):
                 docx_replace_regex(cell, regex, replace)
 
 
-def repl_template(templ_name: str, file_name: str, temps: list, info: list):
+def repl_template(templ_name: str, file_name: str, temps: dict, info: dict):
+    """
+
+    :param templ_name: Имя файла шаблона
+    :param file_name: Имя сохраняемого файла
+    :param temps: заменяемые данные
+    :param info: на что заменяем
+    :return:
+    """
     open_doc = Document(templ_name)
-    dict_ = {key: value for key, value in zip(temps, info)}
+
+    list_tmps = [re.compile(f'<{key}>{value}>') for key, value in temps.items()]
+
+    # txt_doc = d.process(templ_name)
+    # patterns = re.compile(r"{(\w+)}\[(\w+.?\w+)]")
+    # list_tmps = patterns.findall(str(txt_doc))
+
+    dict_ = {key: value for key, value in zip(list(list_tmps), info.values())}
+    print(dict_)
+
     for template, info in dict_.items():
-        print(str(template) + '->' + info)
+        # print(str(template) + '->' + info)
         docx_replace_regex(open_doc, template, info)
     open_doc.save(file_name + '.docx')
 
-def create_template_list(docx_file):
+
+def create_template_dict(docx_file):
     """
     Получение заменяемых переменных из выбранного шаблона
     :param docx_file: Шаблон документа
@@ -79,9 +97,10 @@ def create_template_list(docx_file):
     """
     txt = d.process(docx_file)
 
-    patterns = re.compile(r"{(\w+)}\[(\w+.?\w+)]")
+    patterns = re.compile(r"<(\w+)>(\w+.?\w+)>")
 
     list_patterns = patterns.findall(str(txt))
+    print(list_patterns)
     dict_ = {key: value for key, value in list_patterns}
 
     return dict_
@@ -92,6 +111,8 @@ def input_info(dict_tmps: dict):
     for key, val in dict_tmps.items():
         if val == 'Дата':
             dict_info[key] = create_date()
+        elif val == 'Номер договора':
+            dict_info[key] = create_num(input(f"{val}: "))
         elif val == 'Инициалы':
             dict_info[key] = ''
         else:
@@ -100,12 +121,12 @@ def input_info(dict_tmps: dict):
     if 'initials' in dict_info.keys():
         last_name = dict_info.get('c_ln')
         name_letter = dict_info.get('c_n')[0]
+
         if dict_info.get('c_middn') == '':
             dict_info['initials'] = '.'.join([name_letter, last_name])
         else:
             middle_name = dict_info.get('c_middn')[0]
             dict_info['initials'] = '.'.join([name_letter, middle_name, last_name])
-    print(dict_info)
 
     return dict_info
 
@@ -122,8 +143,19 @@ def get_services(services) -> list:
     return services
 
 
+def get_save_filename(open_tmpfile, inform_dict):
+    filename = '.'.join((open_tmpfile.split('.')[:-1]))
+
+    if 'num' in inform_dict.keys():
+        name_save_doc = ''.join([filename, ' №', inform_dict['num']])
+    else:
+        name_save_doc = filename
+
+    return name_save_doc
+
+
 def main():
-    list_temps = create_template_list()
+    list_temps = create_template_dict()
     list_info = input_info()
 
     tmp_doc = "Шаблон 1.docx"
@@ -132,13 +164,14 @@ def main():
     repl_template(tmp_doc, name_save_doc, list_temps, list_info)
 
 
-
-
-
 if __name__ == '__main__':
-    tmpls = create_template_list(docx_file="Шаблон 1.docx")
-    input_info(tmpls)
+    open_docx_file = "Договор.docx"
 
+    tmpls_dict = create_template_dict(docx_file=open_docx_file)
+    info_dict = input_info(tmpls_dict)
+    saving_filename = get_save_filename(open_docx_file, info_dict)
+
+    repl_template(open_docx_file, saving_filename, tmpls_dict, info_dict)
 
 
 
