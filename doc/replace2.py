61f54ce71ac9bffb7eb3387766346b03abf8e89e
r-elen <elen.ryazanova@gmail.com>
2023-01-08 15:54:13 +0300

    add big code to def docx_replace_regex ToDo Rework!

--- doc/replace2.py
+++ doc/replace2.py
@@ -47,28 +47,114 @@ def create_initials(last_name: str, name: str, middle_name=None):
 
 
 def docx_replace_regex(doc_obj, regex, replace):
-    for p in doc_obj.paragraphs:
-        if regex.search(p.text):
-            text = regex.sub(replace, p.text)
-            style = p.style
-
-            p.text = text
-            p.style = style
-
-            print(p.text)
-            # inline = p.runs
-            # Loop added to work with runs (strings with same style)
-
-            # for i in range(len(inline)):
-            #     if regex.match(inline[i].text):
-            #         text = regex.sub(replace, inline[i].text)
-            #         inline[i].text = text
-            #         print(text)
 
     for table in doc_obj.tables:
         for row in table.rows:
             for cell in row.cells:
                 docx_replace_regex(cell, regex, replace)
+
+    for p in doc_obj.paragraphs:
+        if regex.search(p.text):
+            inline = p.runs
+            # Loop added to work with runs (strings with same style)
+
+            # The text to be replaced can be split over several runs so search through,
+            # identify which runs need to have text replaced then replace the text in those identified
+            started = False
+            key_index = 0
+            # found_runs is a list of (inline index, index of match, length of match)
+            found_runs = list()
+            found_all = False
+            replace_done = False
+
+            for i in range(len(inline)):
+
+                # case 1: found in single run so short circuit the replace
+                if regex.search(inline[i].text) and not started:
+                    print('work1')
+                    found_runs.append((i, inline[i].text.find(regex.pattern), len(regex.pattern)))
+                    text = inline[i].text.replace(regex.pattern, replace)
+                    inline[i].text = text
+                    print(text)
+                    replace_done = True
+                    found_all = True
+                    
+                elif regex.pattern[key_index] not in inline[i].text and not started:
+                    # keep looking ...
+                    print('smth')
+                    # continue
+
+                    start_index = inline[i].text.find(regex.pattern[key_index])
+                    check_length = len(inline[i].text)
+                    for text_index in range(start_index, check_length):
+                        if inline[i].text[text_index] != regex.pattern[key_index]:
+                            break
+                    if key_index == 0:
+                        started = True
+                    chars_found = check_length - start_index
+                    key_index += chars_found
+                    found_runs.append((i, start_index, chars_found))
+                    if key_index != len(regex.pattern):
+                        continue
+                    else:
+                        found_all = True
+                        break
+
+                # case 2: search for partial text, find first run
+                elif regex.pattern[key_index] not in inline[i].text[-1] in regex.pattern and not started:
+                    print('work2')
+                    start_index = inline[i].text.find(regex.pattern[key_index])
+                    check_length = len(inline[i].text)
+                    for text_index in range(start_index, check_length):
+                        if inline[i].text[text_index] != regex.pattern[key_index]:
+                            break
+                    if key_index == 0:
+                        started = True
+                    chars_found = check_length - start_index
+                    key_index += chars_found
+                    found_runs.append((i, start_index, chars_found))
+                    if key_index != len(regex.pattern):
+                        continue
+                    else:
+                        found_all = True
+                        break
+
+                # case 3: search for partial text, find subsequent run
+                elif regex.pattern[key_index] in inline[i].text and started and not found_all:
+                    print('work3')
+                    # check sequence
+                    chars_found = 0
+                    check_length = len(inline[i].text)
+                    for text_index in range(0, check_length):
+                        if inline[i].text[text_index] == regex.pattern[key_index]:
+                            key_index += 1
+                            chars_found += 1
+                        else:
+                            break
+                    # no match so must be end
+                    found_runs.append((i, 0, chars_found))
+                    if key_index == len(regex.pattern):
+                        found_all = True
+                        break
+              
+            if found_all and not replace_done:
+                print('all')
+                for i, item in enumerate(found_runs):
+                    index, start, length = [t for t in item]
+                    if i == 0:
+                        text = inline[index].text.replace(inline[index].text[start:start + length], regex.pattern)
+                        inline[index].text = text
+                    else:
+                        text = inline[index].text.replace(inline[index].text[start:start + length], '')
+                        inline[index].text = text
+            # print(p.text)
+
+            #     if regex.search(inline[i].text):
+            #         text = regex.sub(replace, inline[i].text)
+            #         inline[i].text = text
+            #         print(text)
+
+
 
 
 def repl_template(templ_name: str, file_name: str, temps: dict, info: dict):
