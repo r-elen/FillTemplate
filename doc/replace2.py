539b6f7e7501f33b8c576cfaa921595bad6c1309
r-elen <elen.ryazanova@gmail.com>
2023-01-08 14:19:07 +0300

    replace with NO the same formating

--- doc/replace2.py
+++ doc/replace2.py
@@ -48,14 +48,22 @@ def create_initials(last_name: str, name: str, middle_name=None):
 
 def docx_replace_regex(doc_obj, regex, replace):
     for p in doc_obj.paragraphs:
-        if regex.fullmatch(p.text):
-            inline = p.runs
+        if regex.search(p.text):
+            text = regex.sub(replace, p.text)
+            style = p.style
+
+            p.text = text
+            p.style = style
+
+            print(p.text)
+            # inline = p.runs
             # Loop added to work with runs (strings with same style)
-            for i in range(len(inline)):
-                if regex.search(inline[i].text):
-                    text = regex.sub(replace, inline[i].text)
-                    inline[i].text = text
-                    print(text)
+
+            # for i in range(len(inline)):
+            #     if regex.match(inline[i].text):
+            #         text = regex.sub(replace, inline[i].text)
+            #         inline[i].text = text
+            #         print(text)
 
     for table in doc_obj.tables:
         for row in table.rows:
